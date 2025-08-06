bbe6166b1bc01bdac8f90b9fd172b101b27cab22
Senko <152493636+helpful-fox-senko-san@users.noreply.github.com>
2025-08-06 06:52:55 +0000

    OnlyTurbo

---
+++ Plugin.cs
@@ -0,0 +1,14 @@
+﻿using Dalamud.Plugin;
+namespace OnlyTurbo;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    public Plugin(IDalamudPluginInterface pluginInterface)
+    {
+        DalamudService.Initialize(pluginInterface);
+    }
+
+    public void Dispose()
+    {
+    }
+}
